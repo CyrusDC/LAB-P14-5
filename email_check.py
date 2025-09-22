dbf7f3380be72e540d90969bbc546e21f4f90cca
CyrusDC <cyrusdelcarmen@gmail.com>
2025-09-22 15:50:51 +0800

    Working email_check

--- email_check.py
+++ email_check.py
@@ -2,13 +2,15 @@ import pandas as pd
 
 
 def valid_check(email):
-    if "@" in email and "." in email:
-        return True
+    fake_emails = str(read_csv())
+    if email in fake_emails:
+        return False
     else:
-        return False # Recursively call the function until a valid email is entered
+        return True
 
 def read_csv():
     csvFile = pd.read_csv('dataset/CEAS_08.csv')
-    print(csvFile)
+    sender_dict = dict(csvFile['sender'])
+    return sender_dict
 
-read_csv()
+
